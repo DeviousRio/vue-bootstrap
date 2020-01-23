f3006bd9efe3384e315cbb6079456a7c1a576a34
Radoslav Tsvetkov <geomilevinc@gmail.com>
2020-01-23 10:37:35 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Navigation />
+  </div>
+</template>
+
+<script>
+import Navigation from './components/Navigation.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Home</b-nav-item>
+        <b-nav-item href="#">Login</b-nav-item>
+        <b-nav-item href="#">Register</b-nav-item>
+        <b-nav-item href="#">Logout</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navigation"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
