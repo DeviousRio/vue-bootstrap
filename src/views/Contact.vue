2320ac68acc7592623c91e6c51b180d6533db806
Rio <geomilevinc@gmail.com>
2020-01-30 11:31:44 +0200

    Modified Contact View

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>
-    <h1 class="text-center mt-5">Contact</h1>
-  </div>
+ <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8 text-center">
+          <h2 class="mt-4">Let's Get In Touch!</h2>
+          <hr class="divider my-4">
+          <p class="text-muted mb-5">Ready to start your next project with us? Give us a call or send us an email and we will get back to you as soon as possible!</p>
+        </div>
+      </div>
+      <div class="row mb-5">
+        <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+          <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+          <div>+359 555 444 333 222</div>
+        </div>
+        <div class="col-lg-4 mr-auto text-center">
+          <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+          <!-- Make sure to change the email address in anchor text AND the link below! -->
+          <div>contact@yourwebsite.com</div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -11,5 +28,12 @@ export default {
 </script>
 
 <style scoped>
+  .fa-phone:before,
+  .fa-envelope:before {
+    color: #0056B3;
+  }
 
+  div p {
+    font-size: 25px;
+  }
 </style>
