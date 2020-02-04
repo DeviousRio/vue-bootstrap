928511fcc205abb90a82ceba3de838d4d852bfe2
Rio <geomilevinc@gmail.com>
2020-02-04 11:17:02 +0200

    Modified Login CSS

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -83,52 +83,6 @@ export default {
   padding: 0.5em;
 }
 
-.form-label-group > label {
-  /* position: absolute;
-  top: 0;
-  left: 0;
-  display: block;
-  width: 100%;
-  margin-bottom: 0;
-  line-height: 1.5;
-  color: #495057;
-  border: 1px solid transparent;
-  border-radius: .25rem; */
-  /* transition: all .1s ease-in-out; */
-}
-
-/* .form-label-group input::-webkit-input-placeholder {
-  color: transparent;
-}
-
-.form-label-group input:-ms-input-placeholder {
-  color: transparent;
-}
-
-.form-label-group input::-ms-input-placeholder {
-  color: transparent;
-}
-
-.form-label-group input::-moz-placeholder {
-  color: transparent;
-}
-
-.form-label-group input::placeholder {
-  color: transparent;
-} */
-
-/* .form-label-group input:not(:placeholder-shown) {
-  padding-top: calc(var(--input-padding-y) + var(--input-padding-y) * (2 / 3));
-  padding-bottom: calc(var(--input-padding-y) / 3);
-}
-
-.form-label-group input:not(:placeholder-shown)~label {
-  padding-top: calc(var(--input-padding-y) / 3);
-  padding-bottom: calc(var(--input-padding-y) / 3);
-  font-size: 12px;
-  color: #777;
-} */
-
 .btn-google {
   color: white;
   background-color: #ea4335;
