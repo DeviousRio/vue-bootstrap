2ae12974908b9f9c02c2070f98612c344ba73736
Rio <geomilevinc@gmail.com>
2020-02-06 11:47:47 +0200

    Modified Login and Register Views

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -21,9 +21,6 @@
                             <label class="custom-control-label ml-1" for="customCheck1">Remember password</label>
                         </div>
                         <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
-                        <hr class="my-4">
-                        <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign in with Google</button>
-                        <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
                     </form>
                 </div>
             </div>
@@ -43,12 +40,34 @@ export default {
   border: 0;
   border-radius: 1rem;
   box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+  color: #636363;
+  background: #f2f3f7;
 }
 
 .card-signin .card-title {
   margin-bottom: 2rem;
   font-weight: 300;
   font-size: 1.5rem;
+  position: relative;
+}
+
+.card-signin .card-title:before,
+.card-signin .card-title:after {
+  content: "";
+  height: 2px;
+  width: 30%;
+  background: #d4d4d4;
+  position: absolute;
+  top: 50%;
+  z-index: 2;
+}
+
+.card-signin .card-title:before {
+  left: 0;
+}
+
+.card-signin .card-title:after {
+  right: 0;
 }
 
 .card-signin .card-body {
@@ -61,11 +80,17 @@ export default {
 
 .form-signin .btn {
   font-size: 80%;
-  border-radius: 5rem;
   letter-spacing: .1rem;
   font-weight: bold;
   padding: 1rem;
   transition: all 0.2s;
+  background: #5cb85c;
+  border: none;
+  margin-top: 2em;
+}
+
+.form-signin .btn:hover {
+  background: #28a745;
 }
 
 .form-label-group {
@@ -81,15 +106,5 @@ export default {
 .form-label-group > input,
 .form-label-group > label {
   padding: 0.5em;
-}
-
-.btn-google {
-  color: white;
-  background-color: #ea4335;
-}
-
-.btn-facebook {
-  color: white;
-  background-color: #3b5998;
 }
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="signup-form">
-    <form action="/examples/actions/confirmation.php" method="post">
+    <form>
         <h2>Register</h2>
         <p class="hint-text">Create your account. It's free and only takes a minute.</p>
         <div class="form-group">
@@ -36,29 +36,34 @@ export default {
 </script>
 
 <style scoped>
-.form-control{
+.form-control {
     height: 40px;
     box-shadow: none;
     color: #969fa4;
 }
-.form-control:focus{
+
+.form-control:focus {
     border-color: #5cb85c;
 }
-.form-control, .btn{        
+
+.form-control, .btn {        
     border-radius: 3px;
 }
-.signup-form{
+
+.signup-form {
     width: 450px;
     margin: 0 auto;
     padding: 30px 0;
 }
-.signup-form h2{
+
+.signup-form h2 {
     color: #636363;
     margin: 0 0 15px;
     position: relative;
     text-align: center;
 }
-.signup-form h2:before, .signup-form h2:after{
+
+.signup-form h2:before, .signup-form h2:after {
     content: "";
     height: 2px;
     width: 30%;
@@ -67,17 +72,21 @@ export default {
     top: 50%;
     z-index: 2;
 }	
-.signup-form h2:before{
+
+.signup-form h2:before {
     left: 0;
 }
-.signup-form h2:after{
+
+.signup-form h2:after {
     right: 0;
 }
-.signup-form .hint-text{
+
+.signup-form .hint-text {
     color: #999;
     margin-bottom: 30px;
     text-align: center;
 }
+
 .signup-form form{
     color: #999;
     border-radius: 3px;
@@ -86,13 +95,16 @@ export default {
     box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
     padding: 30px;
 }
+
 .signup-form .form-group{
     margin-bottom: 20px;
 }
-.signup-form input[type="checkbox"]{
+
+.signup-form input[type="checkbox"] {
     margin-top: 3px;
 }
-.signup-form .btn{        
+
+.signup-form .btn {        
     font-size: 16px;
     font-weight: bold;		
     min-width: 140px;
@@ -131,6 +143,6 @@ export default {
 }  
 
 .signup-form  div  a {
-    color: rgb(17, 0, 248);
+    color: #025;
 }
 </style>
