7e3b418f19f79affd973740ce292e4db330acbf1
Rio <geomilevinc@gmail.com>
2020-02-12 13:08:25 +0200

    Modified Contact View

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,5 @@
 <template>
- <div class="container">
+ <div class="container mb-5">
       <div class="row justify-content-center">
         <div class="col-lg-8 text-center">
           <h2 class="mt-4">Let's Get In Touch!</h2>
@@ -7,7 +7,7 @@
           <p class="text-muted mb-5">Ready to start your next project with us? Give us a call or send us an email and we will get back to you as soon as possible!</p>
         </div>
       </div>
-      <div class="row mb-5">
+      <div class="row mb-5 mt-4">
         <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
           <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
           <h3>Talk to Sales</h3>
